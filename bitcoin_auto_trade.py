cb1ff4f8bdf8fa55c102ae7566a56e915fe40757
jsewo9119 <70896242+jsewo9119@users.noreply.github.com>
2023-08-21 00:51:30 +0900

    Update bitcoin_auto_trade.py

--- bitcoin_auto_trade.py
+++ bitcoin_auto_trade.py
@@ -4,12 +4,15 @@ import datetime
 
 access = "LtkkonPLe02FITqDa5HvUiOuAob1wLGHjwAjrMb9"
 secret = "RB9V2SOlScs0KsCdkyMfqYPGLDNza6ruwKJAk1cx"
+
 coin_count = 0
 current_coin =""
+semi_current_coin = ""
 btc = "KRW-BTC"
 xrp = "KRW-XRP"
 eth  = "KRW-ETH"
 coin_list = [btc,xrp,eth]
+upbit = pyupbit.Upbit(access, secret)
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -41,8 +44,33 @@ def get_current_price(ticker):
 def coin_select(num):
     return coin_list[num]
 
+# 최고의 k 값 구하기
+
+def get_ror(k):
+    df = pyupbit.get_ohlcv(semi_current_coin,count = 7)
+    df['range'] = (df['high'] - df['low']) * k
+    df['target'] = df['open'] + df['range'].shift(1)
+
+
+    df['ror'] = np.where(df['high'] > df['target'],
+                         df['close'] / df['target'],
+                         1)
+
+    ror = df['ror'].cumprod()[-2]
+    return ror
+
+def best_ror():
+    ror_list =[]
+    for k in np.arange(0.1, 1.0, 0.1):
+        ror = get_ror(k)
+        ror_list.append(ror)
+    return (ror_list.index(max(ror_list))+1)/10
+
+
+
 # 로그인
-upbit = pyupbit.Upbit(access, secret)
+
+
 print("autotrade start")
 
 # 자동매매 시작
@@ -51,9 +79,10 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time(coin_select(coin_count))
         end_time = start_time + datetime.timedelta(days=1)
-        print('loopStart')
+
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price(coin_select(coin_count), 0.5)
+            semi_current_coin = coin_select(coin_count)
+            target_price = get_target_price(coin_select(coin_count), best_ror())
             current_price = get_current_price(coin_select(coin_count))
             coin_count+=1
             if coin_count >=3:
@@ -63,18 +92,19 @@ while True:
                 if krw > 5000:
                     upbit.buy_market_order(coin_select(coin_count), krw*0.9995)
                     current_coin = coin_select(coin_count)
-                    print("buy")
+                    print("매수")
             else:
-                print('nothing')
+                print('아무일도 없었다')
                 print(coin_select(coin_count))
         else:
             coin = get_balance(current_coin[4:7])
-            coin_quantity = 5000/pyupbit.get_current_price(current_coin)
+            current_price = pyupbit.get_current_price(current_coin)
+            coin_quantity = 5000 / current_price
             if coin > coin_quantity:
                 upbit.sell_market_order(coin_select(coin_count), coin*0.9995)
-                print('sell')
+                print('매도')
         
         time.sleep(1)
     except Exception as e:
-        print(e)
+        print(e,'여기서 버그')
         time.sleep(1)
